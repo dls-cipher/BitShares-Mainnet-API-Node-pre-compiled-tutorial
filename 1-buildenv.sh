d978556b88d26347f0ac1228fc9df30df443053f
Digital (Cipher) Lucifer <39482961+dls-cipher@users.noreply.github.com>
2019-04-28 21:14:14 +0700

    Update 1-buildenv.sh

--- 1-buildenv.sh
+++ 1-buildenv.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 DIR=/
 
-#Preparing Ubuntu 16.04 for default requirements
+#Preparing Ubuntu 16.04 environment
 
-apt-get update && apt-get install gcc-4.9 g++-4.9 cmake make \
+apt-get update && apt-get install gcc-5 g++-5 cmake make \
                      libbz2-dev libdb++-dev libdb-dev \
                      libssl-dev openssl libreadline-dev \
                      autoconf libtool git libcurl4-openssl-dev \
-                     autotools-dev automake build-essential \
+		     autotools-dev automake build-essential \
 		     doxygen libboost-all-dev automake nginx \
-                     g++ libbz2-dev libicu-dev python-dev screen -y
+		     g++ libbz2-dev libicu-dev python-dev screen -y
                      
 apt-get install joe htop
 
@@ -19,17 +19,17 @@ apt-get install unzip
 
 mkdir /bitshares
 
-fallocate -l 30G /bitshares/swap
-chmod 0600 /bitshares/swap
-mkswap /bitshares/swap
-if output=$(swapon /bitshares/swap); then
+fallocate -l 30G /swap
+chmod 0600 /swap
+mkswap /swap
+if output=$(swapon /swap); then
     printf 'SWAP succeded, the output was stored «»\n' "$output"
 fi
 sysctl vm.swappiness=25
 sysctl vm.vfs_cache_pressure=40
 echo "vm.swappiness = 25" | tee -a /etc/sysctl.conf
 echo "vm.vfs_cache_pressure = 40" | tee -a /etc/sysctl.conf
-echo "/bitshares/swap none  swap  sw 0  0" | tee -a /etc/fstab
+echo "/swap none  swap  sw 0  0" | tee -a /etc/fstab
 
 #Reboot in 1 minute
 
