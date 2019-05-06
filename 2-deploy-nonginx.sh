d381158942e571ffce0286971e0c479dc090a739
Digital (Cipher) Lucifer <39482961+dls-cipher@users.noreply.github.com>
2019-05-06 20:57:58 +0700

    Update 2-deploy-nonginx.sh

--- 2-deploy-nonginx.sh
+++ 2-deploy-nonginx.sh
@@ -4,10 +4,15 @@ now=$(date +"%T")
 #Download pre-compiled witness_node software with fully synced blockchain database
 
 cd /bitshares/
-wget https://toronto.ca.api.bitshares.org/downloads/bts-node-full.tar.gz | sed 's/\r//g'
-if output=$(tar zxvf bts-node-full.tar.gz); then
+wget https://toronto.ca.api.bitshares.org/downloads/bts-full-node.tar.gz | sed 's/\r//g'
+if output=$(tar zxvf bts-full-node.tar.gz); then
     printf 'TAR Extract successful on «$now»\n' "$output"
 fi
 rm -rf bts-node-full.tar.gz
 
+echo "##############################################"
+echo "##############################################"
 echo "Downloaded and deployed successfully $now" | tee -a /bitshares/install
+echo "##############################################"
+echo "##############################################"
+
