a8b3f42cf7135b5b12ddea6dbd4ddeea3c14d978
Digital (Cipher) Lucifer <39482961+dls-cipher@users.noreply.github.com>
2019-07-26 15:52:23 +0700

    Update 1-buildenv.sh
    Adding pre command sudo option for Ubuntu users without default root access

--- 1-buildenv.sh
+++ 1-buildenv.sh
@@ -3,7 +3,7 @@ DIR=/
 
 #Preparing Ubuntu 18.10 environment
 
-apt-get update && apt-get install gcc-5 g++-5 cmake make \
+sudo apt-get update && apt-get install gcc-5 g++-5 cmake make \
                      libbz2-dev libdb++-dev libdb-dev \
                      libssl-dev openssl libreadline-dev \
                      autoconf libtool git libcurl4-openssl-dev \
@@ -11,26 +11,26 @@ apt-get update && apt-get install gcc-5 g++-5 cmake make \
 		     doxygen libboost-all-dev automake nginx \
 		     g++ libbz2-dev libicu-dev python-dev screen -y
                      
-apt-get install joe htop
+sudo apt-get install joe htop
 
-apt-get install unzip
+sudo apt-get install unzip
 
 #Building Swap and Structure
 
-mkdir /bitshares
+sudo mkdir /bitshares
 
-fallocate -l 10G /swap #Asuming you are having minimum 8GB of real RAM
-chmod 0600 /swap
-mkswap /swap
-if output=$(swapon /swap); then
+sudo fallocate -l 15G /swap #Asuming you are having minimum 8GB of real RAM
+sudo chmod 0600 /swap
+sudo mkswap /swap
+if output=$(sudo swapon /swap); then
     printf 'SWAP succeded, the output was stored «»\n' "$output"
 fi
-sysctl vm.swappiness=25
-sysctl vm.vfs_cache_pressure=40
-echo "vm.swappiness = 25" | tee -a /etc/sysctl.conf
-echo "vm.vfs_cache_pressure = 40" | tee -a /etc/sysctl.conf
-echo "/swap none  swap  sw 0  0" | tee -a /etc/fstab
+sudo sysctl vm.swappiness=25
+sudo sysctl vm.vfs_cache_pressure=40
+sudo echo "vm.swappiness = 25" | tee -a /etc/sysctl.conf
+sudo echo "vm.vfs_cache_pressure = 40" | tee -a /etc/sysctl.conf
+sudo echo "/swap none  swap  sw 0  0" | tee -a /etc/fstab
 
 #Reboot in 1 minute
 
-shutdown -r +1
+sudo shutdown -r +1
