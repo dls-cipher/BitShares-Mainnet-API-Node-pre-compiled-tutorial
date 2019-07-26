c38c52f1475366918a0572f798831ebefbf359d2
Digital (Cipher) Lucifer <39482961+dls-cipher@users.noreply.github.com>
2019-07-26 15:56:31 +0700

    Update backup-master.sh
    polishing, removal of double copy, rename, structure, versioning

--- backup-master.sh
+++ backup-master.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
 #copy latest witness_node software and print web report
-cp -rfv /bitshares-core/programs/witness_node/ /bts/ > /var/www/html/copy-report.html
+cd /bitshares/bitshares-core/programs/witness_node/ 
+du -h > /var/www/html/file-size-report.html
 #print node version and output as web report
-cd /bts/witness_node/
 ./witness_node -v > /var/www/html/node-version.html
-#print size of the blockchain database as web report
-du -h /bts/witness_node/witness_node_data_dir/blockchain > /var/www/html/size.html
-#pack entire witness node software
 
-cd /bts/
-tar zcvf bts-node-full.tar.gz witness_node/
+
+#pack entire witness node software
+cd /bitshares/bitshares-core/programs/
+tar zcvf bts-precompiled-api.tar.gz witness_node/
 if [ $? -eq 0 ]; then
     echo OK
 else
     echo FAIL
 fi
-rm -rf /bts/witness_node/
-mv /bts/bts-node-full.tar.gz /var/www/html/downloads/
+mv /bitshares/bitshares-core/programs/bts-precompiled-api.tar.gz /var/www/html/downloads/
 
-echo "Script success and download available at https://status200.bitshares.apasia.tech/downloads/bts-node-full.tar.gz"
+echo "Script success and download available at https://toronto.ca.api.bitshares.org/downloads/bts-precompiled-api.tar.gz"
 
